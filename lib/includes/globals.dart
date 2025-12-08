b3631a5e1fcecfd283e35dc8099e4f8897420ca8
Al-Saihan <al.saihan.bafsd.5@gmail.com>
2025-12-08 20:28:46 +0600

    Feat: Added Appbar and Bottom Navigation Bar

---
+++ lib/includes/globals.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color colorTitle = Color.fromARGB(255, 206, 153, 115);
+const Color colorSelectedItem = Color.fromARGB(255, 74, 58, 58);
+const Color colorUnselectedItem = Color.fromARGB(255, 74, 58, 58); 
+const Color colorBackground = Color.fromARGB(255, 228, 190, 158);
